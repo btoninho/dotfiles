ffa1f75678e90bc68cedbaf9f8003ce5bda6c962
bparente <b.toninho@imperial.ac.uk>
2020-11-02 00:31:20 +0000

    Implemented most of the setup file. Missing aquamacs stuff since need to understand how stowing works with individual files.

---
+++ config.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+#Install oh-my-zsh if not present:
+if [[ ! -e $HOME/.oh-my-zsh/oh-my-zsh.sh  ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+#Install stow via homebrew if not present
+if [[ -e stow ]]; then
+    brew install stow
+fi
+
+## neovim configuration ##
+
+#Install vim-plug if not found
+if [[ ! -e $HOME/.local/share/nvim/site/autoload/plug.vim ]]; then
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+#Links for neovim
+stow -v3 -R -t $HOME/ nvim
+
+#Links for zsh
+stow -v3 -R -t $HOME/ zsh
+
+
+
+
